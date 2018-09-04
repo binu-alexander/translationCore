c091abe45677b5384c40bd5e573d689f889f50c6
bspidel <bruce.spidel@gmail.com>
2018-09-04 17:05:09 -0400

    rebuild from v1.0.0 after botched merge

--- scripts/releaseContentUpdate.js
+++ scripts/releaseContentUpdate.js
@@ -1,26 +1,46 @@
-import fs from 'fs-extra';
-import path from 'path';
-import ospath from 'ospath';
 
-const scu = require('tc-source-content-updater');
-
-function updateResources() {
-  const expectedLanguageCodes = ['en', 'hi', 'gk'];
-  const resourcesFolder = path.join(ospath.home(), 'translationCore', 'resources'); 
+const fs = require('fs-extra');
+const path = require('path');
+const ospath = require('ospath');
+const Updater = require('tc-source-content-updater').default;
+const resourcesFolder = path.join(ospath.home(),
+    'translationCore', 'resources-test'); 
+ 
+const updateResources = async function() {
+  const updater = new Updater();
+  const expectedLanguageCodes = ['en', 'hi'];
+ 
   fs.ensureDirSync(resourcesFolder);
 
   if (fs.emptyDir(resourcesFolder)) {
-    // get defaults
-    console.log(resourcesFolder);
+    // get defaults because resource folder is empty
+    console.log("Looking for resources for: " + 
+        expectedLanguageCodes + " to go in: " + resourcesFolder);
+    await updater.downloadResources(
+        expectedLanguageCodes,
+        resourcesFolder )
+    .then( result => {
+      console.log("Resources: ", result);
+    })
+    .catch( err => {
+      console.log("No resources: " + err);
+    });
+    
+  } else {
+
+    let haveLocalResources = updater.getLocalResources();
+    let neededResources = await Updater.getLatestResources(haveLocalResources);
+    
+    Updater.getLatestResources(haveLocalResources, function(res, err) {
+      if(err) {
+        return(err);
+      } else {
+        neededResources = res;
+      }
+    });
   }
-  const haveLocalResources = scu.getLocalResouces(expectedLanguageCodes);
-  const neededResources = scu.getLatestResources(haveLocalResources);
-  const filesToConvert = scu.downloadResources(neededResources);
-  const convertedFiles = scu.convert(filesToConvert);
+  console.log( "updateResources Done.");
+};
 
-  convertedFiles.foreach (function(file) {
-    scu.moveResources(file);
-  });
-}
-
-updateResources();
+const res = updateResources();
+console.log(res);
